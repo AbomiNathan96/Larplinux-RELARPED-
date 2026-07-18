5da8b4c113d6b76fbdd853f95aba02ea938f0a15
AbomiNathan96 <155333743+AbomiNathan96@users.noreply.github.com>
2026-07-18 18:31:10 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -29,43 +29,9 @@ mkdir /mnt/home
 mkdir /mnt/efi
 mount $DISK"1" /mnt/efi
 
-pacstrap -K /mnt base ntfs-3g base-devel linux linux-firmware git grub intel-ucode efibootmgr inotify-tools nano vim vi networkmanager reflector
+pacstrap -K /mnt base ntfs-3g base-devel linux linux-firmware git grub intel-ucode efibootmgr inotify-tools nano vim vi networkmanager reflector wget
 # in classic linux, sound was bad. we are larping old linux sound
 
 genfstab -U /mnt >> /mnt/etc/fstab
+wget 
 arch-chroot /mnt
-ln -sf /usr/share/zoneinfo/Europe/Rome /etc/localtime #i ,, dont know
-hwclock --systohc
-
-echo ""
-echo "ok now i need you again. uncomment the langs you need at minimum en_US.UTF-8. press enter when ready"
-read nothing
-nano /etc/locale.gen
-locale-gen
-touch /etc/locale.conf
-echo "LANG=en_US.UTF-8" > /etc/locale.conf
-touch /etc/hostname
-echo "OK now time for your epic hostname. this is what the network manager will see. impress them."
-read hostname
-echo $hostname > /etc/hostname
-touch /etc/hosts
-echo -e "127.0.0.1 localhost\n
-::1 localhost\n
-127.0.1.1 $hostname" > /etc/hosts
-echo "ok root password time!"
-passwd
-echo "what do you want your username to be?"
-read user
-useradd -mG wheel $user
-echo "ok $user password time!"
-passwd $
-echo ""
-echo -e "I know its been fun and games up until here but this part is important. \n find the line \n uncomment to let members of group wheel execute any command \n This is required for sudo to work."
-read nothing
-EDITOR=nano visudo
-grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=GRUB  
-grub-mkconfig -o /boot/grub/grub.cfg
-systemctl enable NetworkManager
-timedatectl set-ntp true
-#exit#exit the chroot
-#umount -R /mnt
